333e856b7e121bd78414848debc949ffb0559ab8
Elizabeth Gardiner <liz.gardiner@btinternet.com>
2023-12-10 21:00:30 +0000

    Add How I Work page

--- src/components/HowIWork/HowIWork.js
+++ src/components/HowIWork/HowIWork.js
@@ -1,5 +1,27 @@
 import styles from "../HowIWork/HowIWork.module.css";
+import { steps } from "../../data/howIWork";
 
-function HowIWork() {}
+function HowIWork() {
+  return (
+    <section className={styles.stepsSection}>
+      <h2>How I work</h2>
+      <p>
+        No matter how big or small your job, I always work through the same
+        process to make sure the final translation is the best quality it can
+        be. I will only accept your project if I’m 100% confident I can do a
+        good job translating it, and I always proofread my work before delivery.
+        Read below for more details on my translation process.
+      </p>
+      {steps.map((step) => {
+        return (
+          <details className={styles.step}>
+            <summary>{step.title}</summary>
+            <p>{step.text}</p>
+          </details>
+        );
+      })}
+    </section>
+  );
+}
 
 export default HowIWork;
---
+++ src/data/howIWork.js
@@ -0,0 +1,26 @@
+export const steps = [
+  {
+    title: "1) Check the source file",
+    text: "Before committing to any job I ask to see the source file to ensure it’s within my area of specialism. This way I can also see if there is anything that might take more time, for example handwriting, blurry text caused by scanning, or something that needs research. If you aren’t able to show me the source file, I’ll ask for a more detailed explanation of the job. In line with the CIOL code of conduct, I never accept a project if I’m not 100% happy that I can do a good quality translation. ",
+  },
+  {
+    title: "2) Agree rates and deadlines",
+    text: "If I’m happy with the file, I’ll confirm delivery time/date and fee, as well as the procedure for invoicing. ",
+  },
+  {
+    title: "3) Start translating",
+    text: "I use Trados Studio 2021 for most of my work, meaning I benefit from consistent terminology. However I am also more than happy to use other CAT tools, and sometimes (for example if heavy formatting is involved) I prefer to translate the old-fashioned way without any CAT tools. Whilst translating I’m constantly aware of anything that may need extra research, whether that’s an uncommon word I haven’t come across before, or background on a particular topic (a recent example being Greek Mythology!). I pay attention to the context, subject matter and audience to make sure the final translation is a good reflection of the original.",
+  },
+  {
+    title: "4) Have a break",
+    text: "I think it’s important to be able to come back to a first draft with a fresh pair of eyes, so I always make sure there’s time for this. That’s why, if I’m offered a job with a short deadline that I would struggle to meet, I’d rather turn down the job than accept it and risk doing a rushed job.",
+  },
+  {
+    title: "5) Proofread",
+    text: "After a break, I come back to the first draft to proofread. This involves reading through the English to make sure it reads naturally (it should sound like it was written in English originally, and not be obvious that it’s a translation). I also check the translation against the original to make sure everything is there and that I have interpreted the source correctly. This also gives me a final chance to check that terminology is consistent and make sure that the layout matches the original. ",
+  },
+  {
+    title: "6) Deliver",
+    text: "Once I’m happy with my translation, it’s time for delivery. I’ll double check any delivery instructions, like file format and naming conventions, then send it to you via your preferred method. Delivery is always before the deadline and if extenuating circumstances might cause a delay, I will always let you know in advance.",
+  },
+];
